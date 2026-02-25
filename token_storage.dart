11f8aa637f5d9da09995fcbfbd95642445d64ccb
meruyert99 <kozhahmetmikalek@gmail.com>
2026-02-25 20:40:37 +0500

    Create token_storage.dart

---
+++ token_storage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class TokenStorage {
+  static const _key = 'auth_token';
+  final FlutterSecureStorage _storage;
+
+  TokenStorage(this._storage);
+
+  Future<void> saveToken(String token) => _storage.write(key: _key, value: token);
+
+  Future<String?> readToken() => _storage.read(key: _key);
+
+  Future<void> clearToken() => _storage.delete(key: _key);
+}
